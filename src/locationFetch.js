6322f8188d574fd7445620b6e43e418cda4185a8
zhinum <princeonah23@gmail.com>
2026-08-08 21:10:40 +0100

    added a error message incase theres an invalid request

--- src/locationFetch.js
+++ src/locationFetch.js
@@ -1,15 +1,15 @@
 export class locationFetch {
-  async fetchWeather(userLocation) {
+  async fetchWeather(userLocation, displayMessage) {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userLocation}?key=UG4RFWYRU892D4U4TA9NW58WR`;
 
     try {
       const response = await fetch(url);
       if (!response.ok) {
-        throw new Error(`failed to load page error${response.status}`);
+        throw new Error(`Invalid request error ${response.status}`);
       }
       return await response.json();
     } catch (error) {
-      console.log(error.message);
+      displayMessage.textContent = error.message;
     }
   }
 }
