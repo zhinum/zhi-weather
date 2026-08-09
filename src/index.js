0c2a940d06cb14435a2d3efbd5b895e247b39b2f
zhinum <princeonah23@gmail.com>
2026-08-09 06:17:43 +0100

    changed event type to submit, trim for whitespaces linked the ui control with a try catch to get error message displayed

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ import { Uicontrol } from "./uiControl.js";
 
 const location = document.getElementById("location");
 const locationForm = document.getElementById("location-form");
-const displayMessage = document.getElementById("displayMessage");
 
 const weather = new locationFetch();
 const uiController = new Uicontrol();
 
-locationForm.addEventListener("click", async (e) => {
+locationForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const userLocation = location.value;
+
+  const userLocation = location.value.trim();
   if (!userLocation) return;
 
-  const weatherData = await weather.fetchWeather(userLocation, displayMessage);
-  if (weatherData) {
-    console.log(weatherData);
-    uiController.renderUi(weatherData);
+  try {
+    const weatherData = await weather.fetchWeather(userLocation);
+
+    if (weatherData) {
+      console.log(weatherData);
+      uiController.renderUi(weatherData);
+    }
+  } catch (error) {
+    uiController.showError(error.message);
   }
 });
