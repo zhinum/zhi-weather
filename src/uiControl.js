9e16e92c3ba289bc6929f817321432b8cd22f613
zhinum <princeonah23@gmail.com>
2026-08-09 06:13:11 +0100

    feat: changed to css style to toggle display of weather and error message

--- src/uiControl.js
+++ src/uiControl.js
@@ -2,10 +2,21 @@ export class Uicontrol {
   constructor() {
     this.conditions = document.getElementById("condition");
     this.temparature = document.getElementById("temparature");
+    this.displayMessage = document.getElementById("displayMessage");
+    this.weatherDisplay = document.getElementById("weatherDisplay");
   }
   renderUi(weatherReport) {
     this.conditions.textContent = weatherReport.currentConditions.conditions;
     const temp = weatherReport.currentConditions.temp;
     this.temparature.textContent = `${temp}F`;
+
+    this.displayMessage.classList.add("hidden");
+    this.weatherDisplay.classList.remove("hidden");
+  }
+  showError(message) {
+    this.displayMessage.textContent = message;
+
+    this.displayMessage.classList.remove("hidden");
+    this.weatherDisplay.classList.add("hidden");
   }
 }
